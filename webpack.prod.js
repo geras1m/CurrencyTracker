64e02277164bbdb2979ec556b05e84582611f43b
Anton Haldouski <anton94-94@list.ru>
2024-02-28 14:15:55 +0300

    feat: add minimize webpack plugin for build and yarn in a project

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,11 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
+import TerserPlugin from 'terser-webpack-plugin';
 
 module.exports = merge(common, {
   mode: 'production',
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserPlugin()],
+  },
 });
