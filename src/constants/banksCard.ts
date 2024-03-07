e8325ddffaf9861164c4eb80e33d74222c95f70e
Anton Haldouski <anton94-94@list.ru>
2024-03-07 13:37:52 +0300

    feat: add a map for bank card page

---
+++ src/constants/banksCard.ts
@@ -0,0 +1,64 @@
+import { IBankData } from '@root/types';
+
+export const banksList: IBankData[] = [
+  {
+    name: 'Belinvestbank',
+    latitude: 30.33644,
+    longitude: 53.900894,
+    currencies: ['BYN', 'USD'],
+  },
+  {
+    name: 'Belarusbank',
+    latitude: 30.343276,
+    longitude: 53.910405,
+    currencies: ['EUR', 'GEL'],
+  },
+  {
+    name: 'Technobank',
+    latitude: 30.33317,
+    longitude: 53.901181,
+    currencies: ['SEK', 'AUD'],
+  },
+  {
+    name: 'BELVEB Bank',
+    latitude: 30.337023,
+    longitude: 53.898252,
+    currencies: ['UAH', 'INR'],
+  },
+  {
+    name: 'Belagroprombank',
+    latitude: 30.349941,
+    longitude: 53.906612,
+    currencies: ['PLN', 'NOK'],
+  },
+  {
+    name: 'RRB Bank',
+    latitude: 30.333232,
+    longitude: 53.898268,
+    currencies: ['NOK', 'PLN'],
+  },
+  {
+    name: 'BTA Bank',
+    latitude: 30.33741,
+    longitude: 53.898151,
+    currencies: ['INR', 'UAH'],
+  },
+  {
+    name: 'Solution Bank',
+    latitude: 30.334643,
+    longitude: 53.899329,
+    currencies: ['AUD', 'SEK'],
+  },
+  {
+    name: 'Belarusian people s Bank',
+    latitude: 30.343258,
+    longitude: 53.905053,
+    currencies: ['GEL', 'EUR'],
+  },
+  {
+    name: 'Alfa-Bank',
+    latitude: 30.340213,
+    longitude: 53.9098,
+    currencies: ['USD', 'BYN'],
+  },
+];
