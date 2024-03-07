4cc9dfd6ee60176af23085bca2b1b4eabf2d7e25
Anton Haldouski <anton94-94@list.ru>
2024-03-07 13:38:32 +0300

    feat: add a search for bank card page

---
+++ src/components/BankCard/Search/index.tsx
@@ -0,0 +1,69 @@
+import {
+  ElasticSearchContainer,
+  ElasticSearchItem,
+  SearchContainer,
+  SearchImage,
+  SearchInput,
+  SearchInputContainer,
+  SearchTitle,
+} from '@components/BankCard/Search/styled';
+import { CurrenciesList } from '@root/constants';
+import { ChangeEvent, Component } from 'react';
+
+interface ISearchProps {
+  handleSetCurrentCurrency: (currency: string) => void;
+}
+
+interface ISearchState {
+  inputValue: string;
+}
+
+export class Search extends Component<ISearchProps, ISearchState> {
+  constructor(props: ISearchProps) {
+    super(props);
+    this.state = {
+      inputValue: '',
+    };
+  }
+
+  handleOnChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.target;
+    this.setState((state) => ({ ...state, inputValue: value }));
+  };
+
+  getListFilteredCurrencies = (value: string) => {
+    const { handleSetCurrentCurrency } = this.props;
+
+    return CurrenciesList.filter((currency) =>
+      currency.toLowerCase().includes(value.trim().toLowerCase()),
+    ).map((currency) => (
+      <ElasticSearchItem key={currency} onClick={() => handleSetCurrentCurrency(currency)}>
+        {currency}
+      </ElasticSearchItem>
+    ));
+  };
+
+  render() {
+    const { inputValue } = this.state;
+    const filteredCurrencies = this.getListFilteredCurrencies(inputValue);
+
+    return (
+      <SearchContainer>
+        <SearchTitle>Search currency in the bank</SearchTitle>
+        <SearchInputContainer>
+          <SearchInput value={inputValue} onChange={this.handleOnChange} />
+          <SearchImage />
+        </SearchInputContainer>
+        {inputValue.trim().length !== 0 ? (
+          <ElasticSearchContainer>
+            {filteredCurrencies.length === 0 ? (
+              <ElasticSearchItem>Not found</ElasticSearchItem>
+            ) : (
+              filteredCurrencies
+            )}
+          </ElasticSearchContainer>
+        ) : null}
+      </SearchContainer>
+    );
+  }
+}
---
+++ src/components/BankCard/Search/styled.ts
@@ -0,0 +1,78 @@
+import SearchIcon from '@assets/search-icon.svg';
+import { mediaSizes } from '@constants/theme/theme';
+import styled from 'styled-components';
+
+export const SearchContainer = styled.div`
+  max-width: 450px;
+  margin: 0 auto;
+  position: relative;
+`;
+
+export const SearchInputContainer = styled.div`
+  margin-top: 25px;
+  position: relative;
+`;
+
+export const SearchTitle = styled.h2`
+  margin-top: 105px;
+  color: ${({ theme }) => theme.fontCurrencyTitle};
+  font-size: ${({ theme }) => theme.fontSizes.px35};
+  font-weight: ${({ theme }) => theme.fontWeight.normal};
+
+  @media (max-width: ${mediaSizes.maxWidth750px}) {
+    margin-top: 30px;
+    font-size: ${({ theme }) => theme.fontSizes.px24};
+    text-align: center;
+  }
+`;
+
+export const SearchInput = styled.input.attrs({
+  type: 'text',
+  placeholder: 'Сurrency search...',
+})`
+  outline: none;
+  border: none;
+  height: 65px;
+  width: 100%;
+  padding: 0px 50px 0 20px;
+  font-size: ${({ theme }) => theme.fontSizes.px16};
+  font-weight: ${({ theme }) => theme.fontWeight.normal};
+  color: ${({ theme }) => theme.colors.grey2};
+  background-color: ${({ theme }) => theme.colors.black5};
+`;
+
+export const SearchImage = styled(SearchIcon)`
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  right: 20px;
+  margin: auto;
+`;
+
+export const ElasticSearchContainer = styled.ul`
+  position: absolute;
+  z-index: 5;
+  width: 100%;
+  max-height: 500px;
+  color: ${({ theme }) => theme.colors.black};
+  background-color: ${({ theme }) => theme.colors.white2};
+  transition: 0.3s ease-out;
+  padding-bottom: 10px;
+`;
+
+export const ElasticSearchItem = styled.li`
+  list-style-type: none;
+  cursor: pointer;
+  border-bottom: 1px solid ${({ theme }) => theme.colors.grey};
+  margin: 12px 20px 0;
+  transition: 0.1s ease;
+
+  &:last-child {
+    border-bottom: none;
+    margin-bottom: 0;
+  }
+
+  &:hover {
+    color: ${({ theme }) => theme.colors.green1};
+  }
+`;
