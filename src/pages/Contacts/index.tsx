a5496f9b071e4a122abbf10ee14a784e47e0b8e2
Anton Haldouski <anton94-94@list.ru>
2024-03-07 13:42:15 +0300

    feat: add a Contato page

--- src/pages/Contacts/index.tsx
+++ src/pages/Contacts/index.tsx
@@ -1,5 +1,35 @@
+import { Container } from '@components/App/styled';
+import {
+  ContactAddress,
+  ContactItem,
+  ContactLinkMail,
+  ContactLinkPhone,
+  ContactList,
+  ContactUsContent,
+  ContactUsTitle,
+  MainContactUs,
+} from '@pages/Contacts/styled';
+
 const Contacts = () => {
-  return <div>Contacts</div>;
+  return (
+    <MainContactUs>
+      <Container>
+        <ContactUsContent>
+          <ContactUsTitle>Contact Us:</ContactUsTitle>
+          <ContactList>
+            <ContactItem>
+              <ContactLinkMail>contact@modsen-software.com</ContactLinkMail>
+            </ContactItem>
+            <ContactItem>
+              Phone: <ContactLinkPhone>+48501157180</ContactLinkPhone>
+            </ContactItem>
+          </ContactList>
+          <ContactUsTitle>Based in:</ContactUsTitle>
+          <ContactAddress>Warsaw, Poland, Stefana Okrzei 1a/10</ContactAddress>
+        </ContactUsContent>
+      </Container>
+    </MainContactUs>
+  );
 };
 
 export default Contacts;
---
+++ src/pages/Contacts/styled.ts
@@ -0,0 +1,66 @@
+import { mediaSizes } from '@constants/theme/theme';
+import styled from 'styled-components';
+
+export const MainContactUs = styled.main``;
+
+export const ContactUsTitle = styled.h2`
+  margin-bottom: 10px;
+  color: ${({ theme }) => theme.fontCurrencyTitle};
+  font-size: ${({ theme }) => theme.fontSizes.px35};
+  font-weight: ${({ theme }) => theme.fontWeight.normal};
+
+  @media (max-width: ${mediaSizes.maxWidth750px}) {
+    margin-top: 30px;
+    font-size: ${({ theme }) => theme.fontSizes.px24};
+    text-align: center;
+  }
+`;
+
+export const ContactUsContent = styled.div`
+  margin-top: 105px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const ContactList = styled.ul`
+  margin-bottom: 40px;
+`;
+
+export const ContactItem = styled.li`
+  list-style: none;
+  font-size: ${({ theme }) => theme.fontSizes.px24};
+  font-weight: ${({ theme }) => theme.fontWeight.normal};
+  color: ${({ theme }) => theme.colors.grey2};
+
+  @media (max-width: ${mediaSizes.maxWidth450px}) {
+    font-size: ${({ theme }) => theme.fontSizes.px22};
+  }
+`;
+
+export const ContactLinkMail = styled.a.attrs({
+  target: '_blank',
+  href: 'mailto:https://www.modsen-software.com/',
+})`
+  text-decoration: none;
+  color: ${({ theme }) => theme.colors.grey2};
+`;
+
+export const ContactLinkPhone = styled.a.attrs({
+  target: '_blank',
+  href: 'callto:+48501157180',
+})`
+  text-decoration: none;
+  color: ${({ theme }) => theme.colors.grey2};
+`;
+
+export const ContactAddress = styled.p`
+  font-size: ${({ theme }) => theme.fontSizes.px24};
+  font-weight: ${({ theme }) => theme.fontWeight.normal};
+  color: ${({ theme }) => theme.colors.grey2};
+  text-align: center;
+
+  @media (max-width: ${mediaSizes.maxWidth450px}) {
+    font-size: ${({ theme }) => theme.fontSizes.px22};
+  }
+`;
