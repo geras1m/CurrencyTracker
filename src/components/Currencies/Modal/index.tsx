b09484b170bc05666bf681549ab406efdb57cc23
Anton Goldovsky <goldovsky.anton@gmail.com>
2024-03-14 18:04:52 +0300

    feat: add the Portal component

--- src/components/Currencies/Modal/index.tsx
+++ src/components/Currencies/Modal/index.tsx
@@ -1,36 +1,32 @@
 import { CurrencyModalContent } from '@components/Currencies/CurrencyModalContent';
 import { ModalTitle, ModalWindow, ModalWrapper } from '@components/Currencies/Modal/styled';
 import { ModalButton } from '@components/Currencies/styled';
+import { Portal } from '@components/Portal';
 import { useOutsideClick } from '@hooks/useOutClick';
 import { IModalData } from '@root/types';
 import { FC } from 'react';
-import { createPortal } from 'react-dom';
 
 interface IModalProps {
-  isOpen: boolean;
   data: IModalData | null;
   closeModal: () => void;
 }
 
-export const Modal: FC<IModalProps> = ({ isOpen, data, closeModal }) => {
+export const Modal: FC<IModalProps> = ({ data, closeModal }) => {
   const ref = useOutsideClick(closeModal);
 
-  if (!isOpen) {
-    return null;
-  }
-
-  return createPortal(
-    <ModalWrapper data-testid='modal'>
-      {data && (
-        <ModalWindow data-testid='currency-modal' ref={ref}>
-          <ModalTitle data-testid='currency-title'>{data.name}</ModalTitle>
-          <CurrencyModalContent code={data.code} />
-          <ModalButton type='button' onClick={closeModal}>
-            Close modal
-          </ModalButton>
-        </ModalWindow>
-      )}
-    </ModalWrapper>,
-    document.getElementById('modal-root')!,
+  return (
+    <Portal>
+      <ModalWrapper data-testid='modal'>
+        {data && (
+          <ModalWindow data-testid='currency-modal' ref={ref}>
+            <ModalTitle data-testid='currency-title'>{data.name}</ModalTitle>
+            <CurrencyModalContent code={data.code} />
+            <ModalButton type='button' onClick={closeModal}>
+              Close modal
+            </ModalButton>
+          </ModalWindow>
+        )}
+      </ModalWrapper>
+    </Portal>
   );
 };
